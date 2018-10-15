a40c147f15243232d5288671db0af4415076cf63
Joshua Beckford <joshua93beckford@gmail.com>
2018-10-15 19:10:42 -0400

    V 1.0.0

---
+++ deploy/precache-manifest.9e8b4ddb1508c93d3d1f7f27006f64c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7785176ab45deca5e034",
+    "url": "/static/js/main.7785176a.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "7785176ab45deca5e034",
+    "url": "/static/css/main.c0713a7f.chunk.css"
+  },
+  {
+    "revision": "de391119444039b7c3eb95f1d3acfd16",
+    "url": "/index.html"
+  }
+];
